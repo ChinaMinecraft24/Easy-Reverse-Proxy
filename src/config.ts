d3c2b9f476e4b3d6f44fe436c478a06217b91d8f
ChinaMinecraft24 <ChinaMinecraft2023@163.com>
2025-02-06 12:02:36 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,9 @@ export let PORT = 377; // 端口
 
 export let PROXY_CODE_SECRET = 'Easy-Reverse-Proxy'; // 代理码密钥
 // 代理
-export let PROXIES: Proxy[] = [];
+export let PROXIES: Proxy[] = [  
+  {
+    domain: "www.example1.com",
+    url: "https://www.google.com/"
+  }
+];
