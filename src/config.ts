db957f98975af2ad251a8de4704236a2396369aa
ChinaMinecraft24 <ChinaMinecraft2023@163.com>
2025-02-08 13:40:26 +0800

    更新 config.ts

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export let PORT = 377; // 端口
 export let PROXY_CODE_SECRET = 'Easy-Reverse-Proxy'; // 代理码密钥
 // 代理
 export let PROXIES: Proxy[] = [  
-  {domain: "ggproxy-one.vercel.app",url: "https://www.google.com/" }];
-export let proxys: 
-    Proxy[] = [ {domain: "gg.ananyo.fun",url: "https://www.google.com/" }
-];
+ {
+  domain:"easy-reverse-proxy-five.vercel.app/",
+   url: "https://www.google.com.hk/" }
+    ];
